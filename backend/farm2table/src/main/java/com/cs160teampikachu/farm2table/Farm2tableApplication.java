9c80f59e47aff7164821e68f0b879e7e4893c02b
dill-larson <dillonlouislarson@gmail.com>
2020-09-15 11:20:20 -0700

    🎉 initial commit

---
+++ backend/farm2table/src/main/java/com/cs160teampikachu/farm2table/Farm2tableApplication.java
@@ -0,0 +1,13 @@
+package com.cs160teampikachu.farm2table;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Farm2tableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Farm2tableApplication.class, args);
+	}
+
+}
---
+++ backend/farm2table/src/test/java/com/cs160teampikachu/farm2table/Farm2tableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs160teampikachu.farm2table;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Farm2tableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
