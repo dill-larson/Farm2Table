045390a0d588a51ed657459c62915d4e6346c87b
dill-larson <dillonlouislarson@gmail.com>
2020-11-04 18:25:31 -0800

    💄 Changed UI layout to match wireframe mockups

--- frontend/src/components/signup.js
+++ frontend/src/components/signup.js
@@ -1,10 +1,7 @@
 import React /*,{useState, Component}*/ from "react";
-import { Card, Jumbotron} from 'react-bootstrap';
-import {useForm} from "react-hook-form";
-import {auth, generateUserDocument} from "./firebase";
-
-//Image defined
-//import farmland from '../images/dan-meyers-IQVFVH0ajag-unsplash.jpg';
+import { Button, Card, Col, Container, Form, Jumbotron, Row } from 'react-bootstrap';
+import { useForm } from "react-hook-form";
+import { auth, generateUserDocument } from "./firebase";
 
 const createUserWithEmailAndPasswordHandler = async (email, password, displayName, role) => {
 	try{
@@ -22,38 +19,81 @@ export default function SignUp()
     const onSubmit = data => createUserWithEmailAndPasswordHandler(data.email, data.password, data.username, data.role);
 
     return(
-		<div style={{backgroundColor: '#1ABC56', width: '100%', height: '100%'}}>
-			<Jumbotron style={{marginBottom: "100px", backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center", borderTopLeftRadius: "0rem", borderTopRightRadius: "0rem", borderBottomRightRadius: "5rem", borderBottomLeftRadius: "5rem"}}>
+		<div style={{backgroundColor: "#1ABC56"}}>
+			<Jumbotron style={{backgroundColor: "#F9F8F9", backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center", borderTopLeftRadius: "0rem", borderTopRightRadius: "0rem", borderBottomRightRadius: "5rem", borderBottomLeftRadius: "5rem"}}>
 				<h1 className="text-main-brand text-center font-weight-bold display-2">Sign Up</h1>
 			</Jumbotron>
-			<Card className="text-center" style={{width: '30rem', margin: 'auto'}}> 
-				<form onSubmit={handleSubmit(onSubmit)} className="text-left">
-					<h2> New users sign up </h2>
-					<p> User name: <input name="username" ref={register({required: true})} /> 
-					{errors.username && <span>This field is required</span>}</p>
-					
-					<p> Email address: <input name="email" ref={register({required: true })} /> 
-					{errors.email && <span>This field is required</span>} </p>
+            <Container class="bg-white">
+                <Jumbotron style={{backgroundColor: "#F9F8F9", borderRadius: "3rem"}}>
+                    <Form>
+                        <Form.Group controlId="formName">
+                            <Form.Control type="text" placeholder="Name" class="border-0" />
+                        </Form.Group>
+                        <Form.Group controlId="formEmail">
+                            <Form.Control type="email" placeholder="Email Address" class="border-0" />
+                        </Form.Group>
+                        <Form.Group controlId="formPassword">
+                            <Form.Control type="password" placeholder="Password" class="border-0" />
+                        </Form.Group>
+                        <Form.Group controlId="formConfPassword">
+                            <Form.Control type="password" placeholder="Confirm Password" class="border-0" />
+                        </Form.Group>
 
-					<p> Role: <select name="role" ref={register}> 
-						<option value="consumer">Consumer</option>
-						<option value="farmer">Farmer</option>
-					</select></p>
+                        <Form.Group controlId="formGridAddress1">
+                            <Form.Control placeholder="Street Address" />
+                        </Form.Group>
+                        <Form.Group controlId="formGridAddress2">
+                            <Form.Control placeholder="Apartment, studio, or floor" />
+                        </Form.Group>
+                        <Form.Row>
+                            <Form.Group as={Col} controlId="formGridCity">
+                                <Form.Control placeholder="City"/>
+                            </Form.Group>
+                            <Form.Group as={Col} controlId="formGridState">
+                                <Form.Control as="select" defaultValue="State">
+                                    <option>State</option>
+                                    <option>AL</option>
+                                    <option>AK</option>
+                                    <option>AZ</option>
+                                    <option>AR</option>
+                                    <option>CA</option>
+                                    <option>CO</option>
+                                    <option>CT</option>
+                                    <option>DE</option>
+                                    <option>FL</option>
+                                    <option>...</option>
+                                </Form.Control>
+                            </Form.Group>
+                            <Form.Group as={Col} controlId="formGridZip">
+                                <Form.Control placeholder="Zip"/>
+                            </Form.Group>
+                        </Form.Row>
 
-					<p> Password: <input name="password" type="password" ref={register({required: true })} /> 
-					{errors.password && <span>This field is required</span>} </p>
-					<input type="submit" style={{ width:'30rem', position: 'absolute'}} className="btn btn-primary" />
-				</form>
-			</Card> 
+                        <fieldset>
+                            <Form.Group as={Row}>
+                                <Form.Label as="legend" column sm={4}>
+                                    What type of user are you?
+                                </Form.Label>
+                                <Col sm={10}>
+                                    <Form.Check
+                                    type="radio"
+                                    label="Farmer"
+                                    name="roleRadios"
+                                    id="farmerRadio"
+                                    />
+                                    <Form.Check
+                                    type="radio"
+                                    label="Consumer"
+                                    name="roleRadios"
+                                    id="consumerRadio"
+                                    />
+                                </Col>
+                            </Form.Group>
+                        </fieldset>
+                        <Button variant="main-brand" type="submit" style={{width: "100%"}}>Sign Up</Button>
+                    </Form>
+                </Jumbotron>
+            </Container>
 		</div>
     )
 }
-
-
-//REFERENCE code
-/*
-Getting started on react-hook-form 
-https://react-hook-form.com/get-started
-
-https://medium.com/@everdimension/how-to-handle-forms-with-just-react-ac066c48bd4f
-*/
