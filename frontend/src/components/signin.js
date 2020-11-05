71df3aa8af03082dda8e1d11073abcf4d5c92d09
dill-larson <dillonlouislarson@gmail.com>
2020-11-05 14:03:47 -0800

    💄 updated UI to match frontend mockups

--- frontend/src/components/signin.js
+++ frontend/src/components/signin.js
@@ -1,44 +1,78 @@
-import React /*,{useState, Component}*/ from "react";
-import { Card, Jumbotron} from 'react-bootstrap';
-import {useForm} from "react-hook-form";
-import {Link} from 'react-router-dom';
+import React, { Component } from "react";
+import { Button, Col, Container, Form, Jumbotron, Row } from 'react-bootstrap';
 import {auth} from "./firebase";
 
 
-const signInWithEmailAndPasswordHandler = (email, password) => {
-	auth.signInWithEmailAndPassword(email, password).catch(error => {
-		console.error("Error signing in with password and email", error);
-	});
+// const signInWithEmailAndPasswordHandler = (email, password) => {
+// 	auth.signInWithEmailAndPassword(email, password).catch(error => {
+// 		console.error("Error signing in with password and email", error);
+// 	});
 
-	console.log("Current user: "+auth.currentUser.displayName)
-};
+// 	console.log("Current user: "+auth.currentUser.displayName)
+// };
 
-export default function SignIn()
-{
-    const { register, handleSubmit, errors} = useForm();
-	const onSubmit = data => signInWithEmailAndPasswordHandler(data.email, data.password)
-	
-    return(
-        <div style={{backgroundColor: '#1ABC56', width: '100%', height: '100%'}}>
-			<Jumbotron style={{marginBottom: "100px", backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center", borderTopLeftRadius: "0rem", borderTopRightRadius: "0rem", borderBottomRightRadius: "5rem", borderBottomLeftRadius: "5rem"}}>
-				<h1 className="text-main-brand text-center font-weight-bold display-2">Login</h1>
-			</Jumbotron>
-			<Card className="text-center" style={{width: '30rem', margin: 'auto'}}> 
-				<form onSubmit={handleSubmit(onSubmit)} className="text-left">
-					<h2> User login </h2>
-					
-					<p>Email address: <input name="email" ref={register({required: true })} /> 
-					{errors.email && <span>This field is required</span>} </p>
+// const { register, handleSubmit, errors} = useForm();
+// const onSubmit = data => signInWithEmailAndPasswordHandler(data.email, data.password);
 
-					<p>Password: <input name="password" type="password" ref={register({required: true })} /> 
-					{errors.password && <span>This field is required</span>} </p>
-					<Link to={"signup"} className="nav-link" >First time on Farm2App?</Link>
-					
-					<input type="submit" style={{ width:'30rem', position: 'absolute'}} className="btn btn-primary" />
-				</form>
-			</Card> 
-		</div>
-		
-		
-    )
+export default class SignIn extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            User: {
+                id: '',
+                displayName: '',
+                email: ''
+            },
+            password: ''
+        };
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onSubmit() {
+        console.log(this.state);
+        this.siginUser();
+    }
+
+    siginUser() {
+        //TODO
+    }
+    
+    render() {
+        return(
+            <div style={{backgroundColor: "#1ABC56", paddingBottom: "10px"}}>
+                <Jumbotron style={{backgroundColor: "#F9F8F9", backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center", borderTopLeftRadius: "0rem", borderTopRightRadius: "0rem", borderBottomRightRadius: "5rem", borderBottomLeftRadius: "5rem"}}>
+                    <h1 className="text-main-brand text-center font-weight-bold display-2">Sign In</h1>
+                </Jumbotron>
+                <Container class="bg-white">
+                    <Jumbotron style={{backgroundColor: "#F9F8F9", borderRadius: "3rem"}}>
+                        <Form className="mt-n4 mb-n4" onSubmit={(e) => {e.preventDefault(); this.onSubmit();}}>
+                            <Form.Group controlId="formEmail">
+                                <Form.Control 
+                                    type="email"
+                                    placeholder="Email Address"
+                                    value={this.state.User.email}
+                                    onChange={e => {
+                                            var tempUser = this.state.User;
+                                            tempUser.email = e.target.value; 
+                                            this.setState({ User: tempUser });
+                                        }}
+                                    class="border-0" 
+                                />
+                            </Form.Group>
+                            <Form.Group controlId="formPassword">
+                                <Form.Control 
+                                    type="password"
+                                    placeholder="Password" 
+                                    value={this.state.password}
+                                    onChange={e => this.setState({ password: e.target.value })}
+                                    class="border-0"
+                                />
+                            </Form.Group>
+                            <Button className="text-white" variant="main-brand" type="submit" style={{width: "100%"}}>Sign In</Button>
+                        </Form>
+                    </Jumbotron>
+                </Container>
+            </div>
+        );
+    }
 }
