440256277bf38e1005c70ebb8a393967761d4c4b
Mark Fan <markfan@ymail.com>
2020-11-20 23:27:02 -0800

    Update NavigationBar.js

--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -17,32 +17,48 @@ class NavigationBar extends Component {
     }
 
     render() {
-        let userEmail = this.state.user ?
-            this.state.user.email :
-            null
-        return (
-            <Navbar collapseOnSelect  expand="md" style={{backgroundColor: "#F9F8F9"}}>
-                <Navbar.Toggle aria-controls="navbar-nav" /> {/*Responsive Navbar*/}
-                <Navbar.Collapse id="navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link as={Link} to="/" className="text-main-brand">Home</Nav.Link>
-                        <Nav.Link as={Link} to="/market" className="text-main-brand">Marketplace</Nav.Link>
-                    </Nav>
-                    <Nav className="navbar-right">
-                        <Nav.Link as={Link} to="/signup" className="text-main-brand">Sign Up</Nav.Link>
-                        <Nav.Link as={Link} to="/signin" className="text-main-brand">Login</Nav.Link>
-                        <Nav.Link as={Link} to="/cart" className="text-main-brand">Cart</Nav.Link>
-                    </Nav>
-                    <Nav className="navbar-right-logged-in">
-                        <NavDropdown title={userEmail} drop="left" id="nav-profile">
-                            <NavDropdown.Item as={Link} to="/profile">Profile</NavDropdown.Item> {/*Shows on both farmers and consumers*/}
-                            <NavDropdown.Item as={Link} to="/">Farm</NavDropdown.Item> {/*Shows on farmers*/}
-                    
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        )
+		let userEmail = this.state.user ? 
+			this.state.user.email : 
+			null 
+        //console.log("Logged in as " + userEmail);
+		if (typeof userEmail === 'string')
+		{
+			return(
+				<Navbar collapseOnSelect  expand="md" style={{backgroundColor: "#F9F8F9"}}>
+					<Navbar.Toggle aria-controls="navbar-nav" /> {/*Responsive Navbar*/}
+					<Navbar.Collapse id="navbar-nav">
+						<Nav className="mr-auto">
+							<Nav.Link as={Link} to="/" className="text-main-brand">Home</Nav.Link>
+							<Nav.Link as={Link} to="/market" className="text-main-brand">Market</Nav.Link>
+						</Nav>
+						<Nav className="navbar-right-logged-in">
+							<NavDropdown title="{userEmail}" drop="left" id="nav-profile">
+								<NavDropdown.Item as={Link} to="/cart">Cart</NavDropdown.Item> {/*View users cart*/}
+								<NavDropdown.Item as={Link} to="/profile">Profile</NavDropdown.Item> {/*Shows on both farmers and consumers*/}
+								<NavDropdown.Item as={Link} to="/signout">Sign Out</NavDropdown.Item> {/*Sign out users*/}
+							</NavDropdown>
+						</Nav>
+					</Navbar.Collapse>
+				</Navbar>
+				)
+		}
+		else {
+			return (
+				<Navbar collapseOnSelect  expand="md" style={{backgroundColor: "#F9F8F9"}}>
+					<Navbar.Toggle aria-controls="navbar-nav" /> {/*Responsive Navbar*/}
+					<Navbar.Collapse id="navbar-nav">
+						<Nav className="mr-auto">
+							<Nav.Link as={Link} to="/" className="text-main-brand">Home</Nav.Link>
+							<Nav.Link as={Link} to="/market" className="text-main-brand">Market</Nav.Link>
+						</Nav>
+						<Nav className="navbar-right">
+							<Nav.Link as={Link} to="/signup" className="text-main-brand">Sign Up</Nav.Link>
+							<Nav.Link as={Link} to="/signin" className="text-main-brand">Login</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Navbar>
+			)
+		}
     }
 }
 
