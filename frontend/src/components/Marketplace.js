35c10e483df47c6ae6127ffeeb7c06d185717f97
dill-larson <dillonlouislarson@gmail.com>
2020-11-29 20:54:03 -0800

    💄 wrapped productList in a container

--- frontend/src/components/Marketplace.js
+++ frontend/src/components/Marketplace.js
@@ -1,9 +1,7 @@
 import React from 'react';
+import { Container, Jumbotron } from 'react-bootstrap';
 import ProductList from './ProductList';
 import SearchBox from './SearchBox';
-import { Jumbotron} from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import { getProducts } from '../services/product.service';
 
 class Marketplace extends React.Component{
@@ -35,25 +33,26 @@ class Marketplace extends React.Component{
     }
 
     render(){
-        console.log(this.state.products);
-        console.log(this.state.products.length);
-        const{products: products, searchfield} = this.state
-        const filteredProducts = products.filter(product =>{
+        const { products: products, searchfield } = this.state;
+        const filteredProducts = products.filter(product => {
             return product.name.toLocaleLowerCase().includes(searchfield.toLocaleLowerCase());
-        })
+        });
         return !filteredProducts.length ?
         <h1>Loading</h1> :
-       ( <div className= "text-center" style= {{backgroundColor: "#1ABC56"}}>
-            <Jumbotron style={{ backgroundColor: "#F9F8F9", borderBottomRightRadius: "5rem", borderBottomLeftRadius: "5rem"}}>
-            <h1 className="text-main-brand text-center font-weight-bold display-2"> Marketplace</h1>
-            <div style= {{display: "flex", flexDirection:"row", justifyContent: "center", alignContent: "center"}}>
-            <p style= {{paddingTop:"13px"}}>Search Products:</p><SearchBox searchChange={this.onSearchChange}/>
+        ( 
+            <div style= {{backgroundColor: "#1ABC56", paddingBottom: "10px"}}>
+                <Jumbotron style={{backgroundColor: "#F9F8F9", backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center", borderTopLeftRadius: "0rem", borderTopRightRadius: "0rem", borderBottomRightRadius: "5rem", borderBottomLeftRadius: "5rem"}}>
+                    <h1 className="text-main-brand text-center font-weight-bold display-2"> Marketplace</h1>
+                    <div style= {{display: "flex", flexDirection:"row", justifyContent: "center", alignContent: "center"}}>
+                        <p style= {{paddingTop:"13px"}}>Search Products:</p><SearchBox searchChange={this.onSearchChange}/>
+                    </div>
+                </Jumbotron>
+                <Container>
+                    <ProductList products = {filteredProducts}/>
+                </Container>
             </div>
-            </Jumbotron>
-                <ProductList products = {filteredProducts}/>
-        </div>
         );
-         }
+    }
 }
 
 export default Marketplace;
