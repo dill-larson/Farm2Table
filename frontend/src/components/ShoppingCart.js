4696f3dfb8e682e8f79b739d30b74b8fe8b4864b
dill-larson <dillonlouislarson@gmail.com>
2020-11-30 17:17:12 -0800

    cleaned up code

--- frontend/src/components/ShoppingCart.js
+++ frontend/src/components/ShoppingCart.js
@@ -9,12 +9,10 @@ import { getCartItems } from "../services/cart.service";
 
 class ShoppingCart extends React.Component{
     constructor(){
-        super()
+        super();
         this.state = {
-            cart: [],
-            products: [],
-            searchfield: ''
-        }
+            products: []
+        };
     }
 
     componentDidMount(){
@@ -28,30 +26,25 @@ class ShoppingCart extends React.Component{
             })
             .catch(error => {
                 console.log(error);
-            })
+            });
     }
 
     render(){
-        const { products: products, searchfield } = this.state;
-        const filteredProducts = products.filter(product =>{
-            return product.name.toLocaleLowerCase().includes(searchfield.toLocaleLowerCase());
-        })
-
-        return !products.length ?
-        <h1>Cart is Empty</h1> :
+        const { products: products } = this.state;
+        return !products.length ? <h1>Cart is Empty</h1> :
         (
-        <div style= {{backgroundColor: "#1ABC56"}}>
-            <Jumbotron style={{ backgroundColor: "#F9F8F9", borderBottomRightRadius: "5rem", borderBottomLeftRadius: "5rem"}}>
-            <h1 className=" text-center text-main-brand font-weight-bold display-2"> Shopping Cart</h1>
-            </Jumbotron>
-            <div  style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
-            <ShoppingCartList products = {filteredProducts}/>
+            <div style= {{backgroundColor: "#1ABC56"}}>
+                <Jumbotron style={{ backgroundColor: "#F9F8F9", borderBottomRightRadius: "5rem", borderBottomLeftRadius: "5rem"}}>
+                    <h1 className=" text-center text-main-brand font-weight-bold display-2"> Shopping Cart</h1>
+                </Jumbotron>
+                <div  style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+                    <ShoppingCartList products = {products}/>
+                </div>
+                <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+                    <div style= {{paddingTop: "50rem"}}></div>
+                    <Checkout></Checkout>
+                </div>
             </div>
-            <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
-            <div style= {{paddingTop: "50rem"}}></div>
-            <Checkout></Checkout>
-            </div>
-        </div>
         );
         }
 }
