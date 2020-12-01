19a75f65a2fbe39aacb5ace9e24dab2256455002
dill-larson <dillonlouislarson@gmail.com>
2020-11-30 17:17:03 -0800

    💄 updated table items

--- frontend/src/components/CartItem.js
+++ frontend/src/components/CartItem.js
@@ -2,27 +2,22 @@ import React from 'react';
 import 'tachyons';
 import Table from 'react-bootstrap/Table'
 
-const ProductCard = ({id,name, farm, distance, stock, price}) => {
+const ProductCard = ({id, name, quantity, price}) => {
     return(
-            <tr>
-                <td> <p>{id}</p></td>
-                <td style={{display:"flex", flexDirection: "row"} }> 
-                    <img style={{height: "15%", width: "15%", padding:"2%"}} alt='products' src={`https://robohash.org/${id}?200x200`}/>
-                    <div>
-                    <h5 style={{color:"#D08737"}}> {name}</h5>
-                    <p> {farm} farm</p>
-                    </div>
-               </td>
-               <td>
-                    <p>${price}</p>
-               </td>
-               <td>
-                    <p>{stock}</p>
-               </td>
-               <td>
-               <button className="btn btn-secondary" id="addToCart">Remove</button>
-               </td>
-            </tr>
+        <tr>
+            <td>
+                {quantity}
+            </td>
+            <td> 
+                <h5 className="text-main-brand">{name}</h5>
+            </td>
+            <td>
+                <p>${price}</p>
+            </td>
+            <td>
+            <button className="btn btn-secondary" id="addToCart">Remove</button>
+            </td>
+        </tr>
     );
 }
 
