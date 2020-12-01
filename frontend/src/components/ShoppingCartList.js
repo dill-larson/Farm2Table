fa44766e0bc834ddecb94888e2a550e956763d45
dill-larson <dillonlouislarson@gmail.com>
2020-11-30 17:17:21 -0800

    💄 change table headers

--- frontend/src/components/ShoppingCartList.js
+++ frontend/src/components/ShoppingCartList.js
@@ -5,39 +5,33 @@ import Table from 'react-bootstrap/Table'
 const ShoppingCartList = ({products}) => {
     return(
         <div class = "rounded" style={{border: "0.5px solid LightGray",backgroundColor: "#F9F8F9", width: "95%"}}>
-        <Table  hover >
-             <thead>
-                <tr style= {{color:"#E09866"}}>
-                    <th>#</th>
-                    <th>Product</th>
-                    <th>Price</th>
-                    <th>Quantity</th>
-                    <th>Remove</th>
-                </tr>
-            </thead>
-        <tbody>
-
-
-            {
-            products.map(product => {
-            return (
-                <CartItem 
-                key={product.id}
-                id={product.id}
-                name={product.name}
-                price={product.id}
-                farm={product.name}
-                distance={product.id}
-                stock={product.id}
-                />
-
-            );
-             })
-            }
+            <Table hover>
+                <thead>
+                    <tr style= {{color:"#E09866"}}>
+                        <th>Quantity</th>
+                        <th>Product</th>
+                        <th>Price</th>
+                        <th>Remove</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        products.map(product => {
+                            return (
+                                <CartItem 
+                                    key={product.id}
+                                    id={product.id}
+                                    name={product.name}
+                                    price={product.price}
+                                    quantity={product.quantity}
+                                />
+                            );
+                        })
+                    }
                 </tbody>
-        </Table>
+            </Table>
         </div> 
-            );
+    );
 }
 
 export default ShoppingCartList;
