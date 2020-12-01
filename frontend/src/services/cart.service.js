131c123793b86aa92a164044b103c666fe570f7e
Dillon Larson <61177185+dill-larson@users.noreply.github.com>
2020-11-30 16:18:17 -0800

    Bugfix: Backend Carts (#24)
    * ✨ added function to create user cart
    
    * 🐛 createUserData now also creates user cart

--- frontend/src/services/cart.service.js
+++ frontend/src/services/cart.service.js
@@ -1,5 +1,21 @@
 import { firestore } from "../components/firebase";
 
+/*
+ * Helper function: Creates user cart, so products can be added
+ * @user: Firebase Auth user
+ * return: Promise
+ */
+export async function createUserCart(user) {
+    const cartRef = firestore.doc(`carts/${user.uid}`);
+
+    const data = {
+        id: user.uid,
+        quantity: 0
+    }
+
+    cartRef.set(data);
+}
+
 export async function getCartProductDocs(uid) {
     const cartsRef = firestore.collection('carts');
     return cartsRef.where('user', '==', uid).limit(1).get().then( (querySnapshot) => {
